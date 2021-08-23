a1fcd3b85f65fe432d7c4634ed9efbab101c021a
Marcello Barile <marcello.barile@gmail.com>
2021-08-23 14:49:55 +0200

    renamed library

--- projects/ngx-ksss/karma.conf.js
+++ projects/ngx-ksss/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, '../../coverage/ksss'),
+      dir: require('path').join(__dirname, '../../coverage/ngx-ksss'),
       subdir: '.',
       reporters: [
         { type: 'html' },
